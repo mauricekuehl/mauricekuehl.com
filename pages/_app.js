a4c3d36e9ae83ff1d878fc687f4d8fdd1709b690
maurice kuehl <maurice.kuehl@protonmail.com>
2022-05-19 17:58:16 +0200

    next setup

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import "../styles/globals.css";
+
+function MauriceKuehlCom({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MauriceKuehlCom;
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Maurice Kuehl</title>
+        <meta name="description" content="Maurice Kuehl personal website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}></main>
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+}
