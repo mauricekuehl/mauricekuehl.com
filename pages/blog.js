e235f2e56c0fea0d8e2edb0cc6e0add5cd18fdf2
maurice kuehl <maurice.kuehl@protonmail.com>
2022-05-26 19:47:27 +0200

    added blog styles

--- pages/blog.js
+++ pages/blog.js
@@ -11,24 +11,35 @@ export default function Blog({ allPostsData }) {
         <meta name="description" content="Maurice Kuehl personal blog" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section>
-        {" "}
-        <h1>Maurice Kuehl&apos;s Blog</h1>
-        <p>i am .. and i right about ... ?</p>
-      </section>
-      <section>
-        {allPostsData.map(({ id, date, title }) => (
-          <li className={styles.listItem} key={id}>
-            <Link href={`/blog/${id}`}>
-              <a>{title}</a>
-            </Link>
-            <br />
-            <small className={styles.lightText}>
-              {new Date(date).toDateString()}
-            </small>
-          </li>
-        ))}
-      </section>
+      <main className={styles.main}>
+        <section>
+          {" "}
+          <h1 className={styles.heading}>Maurice Kuehl&apos;s Blog</h1>
+          <p className={styles.blogDescription}>
+            i am .. and i right about ... ? Ich werde reich sein btw ich brauche
+            hier nur dummie text und weiß nicht was ich schreiebn soll xDi am ..
+            and i right about ... ? Ich werde reich sein btw ich brauche hier
+            nur dummie text und weiß nicht was ich schreiebn soll xDi am .. and
+            i right about ... ? Ich werde reich sein btw ich brauche hier nur
+            dummie text und weiß nicht was ich schreiebn soll xDi am .. and i
+            right about ... ? Ich werde reich sein btw ich brauche hier nur
+            dummie text und weiß nicht was ich schreiebn soll xD
+          </p>
+        </section>
+        <section>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={styles.listItem} key={id}>
+              <Link href={`/blog/${id}`}>
+                <a className={styles.listHeading}>{title}</a>
+              </Link>
+              <br />
+              <small className={styles.lightText}>
+                {new Date(date).toDateString()}
+              </small>
+            </li>
+          ))}
+        </section>
+      </main>
     </div>
   );
 }
--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,15 +1,17 @@
 import Head from "next/head";
 import { getAllPostIds, getPostData } from "../../lib/posts";
-// import styles from "../../styles/Blog.module.scss";
+import styles from "../../styles/Blog.module.scss";
 
 export default function Post({ postData }) {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>{postData.title}</title>
+        <meta name="description" content="Maurice Kuehl personal blog" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <article>
-        <h1>{postData.title}</h1>
+      <article className={styles.article}>
+        <h1 className={styles.heading}>{postData.title}</h1>
         <div>{new Date(postData.date).toDateString()} </div>
         {/*  eslint-disable-next-line react/no-danger */}
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
